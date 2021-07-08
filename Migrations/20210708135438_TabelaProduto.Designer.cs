6e5ea0f698b63fbb3f9a9ea41303d008e4f67ca4
Antonio Tenorio <antonio.v.tenorio@outlook.com>
2021-07-08 12:09:06 -0300

    created product migration, product model and view

--- Migrations/20210708135438_TabelaProduto.Designer.cs
+++ Migrations/20210708135438_TabelaProduto.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcExample;
 
 namespace MvcExample.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210708135438_TabelaProduto")]
+    partial class TabelaProduto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,15 +1,18 @@
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcExample
 {
-    public class Categoria
+    public class Produto
     {
         public int Id { get; set; }
         [Display(Name ="Descrição")]
         [Required(ErrorMessage = "O campo descrição é obrigatório")]
         public string Descricao { get; set; }
+        [Range(1, 10,ErrorMessage = "Valor fora da faixa {0}, {1}")]
+        public int Quantidade { get; set; }
         
-        public List<Produto> Produtos { get; set; }
+        public int CategoriaId { get; set; }
+        
+        public Categoria Categoria {get; set;}
     }
 }
